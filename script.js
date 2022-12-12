773c01c44356dbe6d6ba3c4b1ffd2cfabc8eaf04
zaidfadel-ceq <zaid.fadel@cloudeq.com>
2022-12-12 08:04:42 -0600

    done with form

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
 
+
+
+
+  
+
+
+
+
+
+
+
+
+
+jQuery(document).ready(function($) {
+
+    if (sessionStorage.getItem('advertOnce') !== 'true') {
+      $('.box').show();
+    } else {
+      $('.box').hide();
+    };
+  
+    $('.btn-alpha').click(function() {
+      sessionStorage.setItem('advertOnce', 'true');
+      $('.box').hide();
+    });
+  
+    $('.btn-beta').click(function() {
+      window.location.href = 'http://google.com/';
+  
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 
